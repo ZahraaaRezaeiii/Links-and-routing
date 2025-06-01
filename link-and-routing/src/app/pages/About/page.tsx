cd86a3e439aafe23d1d463d621507e9cbe742337
Zahra Rezaei <zahraarezaeii@outlook.com>
2025-06-01 12:10:40 +0330

    first commit

---
+++ link-and-routing/src/app/pages/About/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const About = () => {
+    return (
+        <>
+            <div>About page</div>
+            <Link href="./Home">Go to Home page</Link>
+        </>
+    )
+}
+
+export default About
---
+++ link-and-routing/src/app/pages/Home/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Home = () => {
+    return (
+        <>
+            <div>Home Page</div>
+            <Link href="./About">Go to About page</Link>
+        </>
+    )
+}
+
+export default Home 
